4f568e084412d8106522b4796a8bcafc33c9a2c1
MustafaXD <73116655+MustafaTheCoder@users.noreply.github.com>
2022-06-13 17:36:55 +0500

    eeeeeeeee

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import os
 import tempfile
 import tkinter as tk
@@ -7,8 +6,11 @@ import zipfile
 from elevate import elevate
 from pyquery import PyQuery as pq
 from pyshortcuts import make_shortcut
+import customtkinter
 
 window = tk.Tk()
+window.iconbitmap("assets/Favicon.ico")
+window['background'] ='#FFFFFF'
 elevate(show_console=False)
 
 
@@ -29,12 +31,12 @@ def Download(url: str, path: str, action: tk.Label):
 
     with zipfile.ZipFile(tmpfile, "r") as zip_ref:
         zip_ref.extractall(path)
-    action.config(text="Setting up...")
+    action.config(text="Setting Up...")
     shutil.rmtree(tmpdir)
     make_shortcut(os.path.join(path, "Octave", "Octave.exe"), "Octave",
                   None, None, None, False, True, True),
     action.config(text="Done!")
-    action.after(5000, lambda: window.destroy())
+    action.after(5000, lambda: window.destroy()) 
 
 
 def PickDir(initial: str, dir_set: tk.Entry):
@@ -52,15 +54,21 @@ def Stage1():
     Create the basic setup of the display
     """
 
-    box = tk.Frame(width=500, height=300)
+    box = tk.Frame(width=500, height=300, bg="white")
     box.pack()
 
-    label = tk.Label(box, text="Octave", font=("Arial Bold", 50))
-    label.pack()
+    label = tk.Label(box, text="Install Octave", font=("Poppins", 25),bg="white")
+    label.pack(padx=20, pady=10)
 
-    startbutton = tk.Button(box, text="Start", width=10,
-                            height=2, command=Stage2)
-    startbutton.pack()
+    button = customtkinter.CTkButton(width=120,
+                                    height=32,
+                                    fg_color="#00D4FF", 
+                                    hover_color="#1ECEF1",
+                                    border_width=0,
+                                    corner_radius=8,
+                                    text="Start Installation",
+                                    command=Stage2)
+    button.pack(padx=20, pady=10)
 
 
 def Stage2():
@@ -71,25 +79,25 @@ def Stage2():
     for child in ch:
         child.destroy()
 
-    box = tk.Frame(width=500, height=300)
+    box = tk.Frame(width=500, height=300, bg="white")
     box.pack()
 
-    label = tk.Label(box, text="Octave", font=("Arial Bold", 30))
-    label.pack()
+    label = tk.Label(box, text="Install Octave", font=("Poppins", 25), bg="white")
+    label.pack(padx=20, pady=10)
 
-    dir_set = tk.Entry(box, width=50)
+    dir_set = tk.Entry(box, width=50, bg="#00D4FF")
     #default_dir = r"C:\Program Files (x86)\Octave"
     default_dir = r"C:\Program Files"
     dir_set.insert(0, default_dir)
-    dir_set.pack()
+    dir_set.pack(padx=20, pady=10)
 
     dir_picker = tk.Button(box, text="📁", width=10,
-                           command=lambda: PickDir(default_dir, dir_set))
-    dir_picker.pack()
+                           command=lambda: PickDir(default_dir, dir_set), bg="#00D4FF")
+    dir_picker.pack(padx=20, pady=10)
 
     next_button = tk.Button(box, text="Next", width=10,
-                            command=lambda: Stage3(dir_set.get()))
-    next_button.pack()
+                            command=lambda: Stage3(dir_set.get()), bg="#00D4FF")
+    next_button.pack(padx=70, pady=60)
 
 
 def Stage3(path: str):
@@ -105,10 +113,10 @@ def Stage3(path: str):
         os.makedirs(path)
     d = pq(url="https://github.com/ReCore-sys/Octave/releases")
     link = "https://github.com" + d(".Box-row > div > a").attr("href")
-    title_lable = tk.Label(window, text="Octave", font=("Arial Bold", 50))
+    title_lable = tk.Label(window, text="Octave", font=("Arial Bold", 50), bg="white")
     title_lable.pack()
     current_action = tk.Label(
-        window, text="Downloading...", font=("Arial Bold", 30))
+        window, text="Downloading...", font=("Arial Bold", 30), bg="white")
     current_action.pack()
     Download(link, path, current_action)
 
